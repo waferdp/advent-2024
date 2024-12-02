196476776a7de479dd8f36c51ea97118d93016e9
Jacob Brännström <jacob.brannstrom@motorit.se>
2024-12-02 19:26:01 +0100

    Initial commit
    Solutions for day 1 & 2, both parts

---
+++ 01/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.IO.Compression;
+
+namespace advent.day1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var example = new Program().GetNumbers("example.txt");
+            var numbers = new Program().GetNumbers("input.txt");
+            // Console.WriteLine(Part1(numbers));
+            Console.WriteLine(Part2(numbers));
+        }
+
+        private (List<int>, List<int>) GetNumbers(string filename)
+        {
+            var lines = File
+                .ReadAllLines(filename)
+                .Select(r => r
+                    .Split(" ")
+                    .Where(s => !string.IsNullOrWhiteSpace(s))
+                    .Select(int.Parse)
+                    .ToList()).ToList();
+            var a = lines.Select(l => l[0]).ToList();
+            var b = lines.Select(l => l[1]).ToList();
+            a.Sort();
+            b.Sort();
+            return (a, b);
+        }
+
+        static int Part1((List<int> a, List<int> b) numbers)
+        {
+            var a = numbers.a;
+            var b = numbers.b;
+            var i = 0;
+            var sum = 0;
+            while (i < a.Count && i < b.Count)
+            {
+                sum += Math.Abs(a[i] - b[i]);
+                i++;
+            }
+            return sum;
+        }
+
+        static int Part2((List<int> a, List<int> b) numbers)
+        {
+            var a = numbers.a;
+            var b = numbers.b;
+            var i = 0;
+            var sum = 0;
+            while (i < a.Count && i < b.Count)
+            {
+                var left = a[i];
+                var count = b.Count(r => r == left);
+                sum += left * count;
+                i++;
+            }
+            return sum;
+        }
+    }
+}
---
+++ 02/Program.cs
@@ -0,0 +1,81 @@
+﻿namespace _02;
+
+public class Program
+{
+    private readonly List<List<int>> input = [];
+    
+    public Program(string filename)
+    {
+        input = ParseInput(filename);
+    }
+
+    public long Part1()
+    {
+        return input.Select(IsSafe).Count(x => x);
+    }
+
+    public long Part2()
+    {
+        return input.Select(IsSafeWithDampener).Count(x => x);
+    }
+
+    public static void Main(string[] args)
+    {
+        var program = new Program("input.txt");
+        //var program = new Program("example.txt");
+        Console.WriteLine(program.Part1());
+        Console.WriteLine(program.Part2());
+    }
+
+    private bool IsSafe(List<int> numbers)
+    {
+        var incDec = numbers[1].CompareTo(numbers[0]);
+        for(var i = 1; i < numbers.Count; i++)
+        {
+            var a = numbers[i - 1];
+            var b = numbers[i];
+
+            var diff = Math.Abs(b - a);
+            if (diff < 1 || diff > 3 )
+            {
+                return false;
+            }
+
+            if (b.CompareTo(a) != incDec)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private bool IsSafeWithDampener(List<int> numbers)
+    {
+        for(var i = 0; i < numbers.Count; i++)
+        {
+            var removed = numbers[0..i].Concat(numbers[(i+1)..]).ToList();
+            if(IsSafe(removed))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private List<List<int>> ParseInput(string filename)
+    {
+        var lines = File.ReadAllLines(filename);
+        var result = new List<List<int>>();
+        foreach (var line in lines)
+        {
+            var numbers = line
+                .Split(' ')
+                .Where(s => !string.IsNullOrEmpty(s))
+                .Select(int.Parse).ToList();
+
+            result.Add(numbers);
+        }
+
+        return result;
+    }
+}
